dcd4e91fc6fbbd3157f5878f104c6d70360ab5ea
kireto <k.mitov@robopartans.com>
2017-01-29 10:29:21 +0200

    Clearer message

--- config/initializers/app_assets.rb
+++ config/initializers/app_assets.rb
@@ -1,7 +1,8 @@
-puts "In app_assets:"
+puts ">> Start: Assets after initialize:"
 Rails.application.config.after_initialize do |app|
 	puts app.assets.class
 	app.assets.paths.each do |path|
 		puts path
 	end
 end
+puts "<< End: Assets after initialize"
